c04d28ef89a9fd24e132f31165ac7a6cfb2a8065
rannel.ribeiro <95056075+rannel11@users.noreply.github.com>
2022-04-20 13:06:52 -0300

    Add files via upload

---
+++ Landingpage Rannel/script.js
@@ -0,0 +1,2 @@
+function mostrarOcultarSenha(){var a=document.getElementById("senha");a.type="password"==a.type?"text":"password"}var email=document.getElementById("email");email.addEventListener("focus",()=>{email.style.borderColor="#7C0015"});
+
